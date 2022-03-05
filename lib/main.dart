7a41187bfcce134ef25eaa87c7abc6b2228c3618
amir58 <devamir11@gmail.com>
2022-03-05 18:06:35 +0200

    Show notifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutterfirecourse/business_logic/cubits/add_post/add_post_cubit.dart';
 import 'package:flutterfirecourse/business_logic/cubits/chatting/chatting_cubit.dart';
 import 'package:flutterfirecourse/business_logic/cubits/comments/comments_cubit.dart';
@@ -18,17 +20,42 @@ void main() async {
   await MyShared.init();
   await Firebase.initializeApp();
 
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+
+  await flutterNotification
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
   runApp(DevicePreview(
     enabled: false,
     builder: (context) => MyApp(),
   ));
 }
 
-class MyApp extends StatelessWidget {
+var flutterNotification = FlutterLocalNotificationsPlugin();
+
+AndroidNotificationChannel channel = const AndroidNotificationChannel(
+  "id",
+  "name",
+  description: "desc",
+  importance: Importance.high,
+  playSound: true,
+);
+
+class MyApp extends StatefulWidget {
   MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
 
+class _MyAppState extends State<MyApp> {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
@@ -60,5 +87,38 @@ class MyApp extends StatelessWidget {
             builder: (p0, p1, p2) => SplashScreen(),
           ),
         ));
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    FirebaseMessaging.instance.getToken().then((value) => print(value));
+
+    FirebaseMessaging.onMessage.listen((event) {
+      print('listen => ');
+      print('listen => ${event.notification!.title}');
+      print('listen => ${event.notification!.body}');
+      print('listen => ${event.notification!.android}');
+      print('listen => ${event.notification!.apple}');
+
+      if (event.notification != null && event.notification!.android != null) {
+        flutterNotification.show(
+          event.notification.hashCode,
+          event.notification?.title,
+          event.notification?.body,
+          NotificationDetails(
+            android: AndroidNotificationDetails(
+              channel.id,
+              channel.name,
+              channelDescription: channel.description,
+              color: Colors.blue,
+              playSound: true,
+              icon: '@mipmap/ic_launcher',
+            ),
+          ),
+        );
+      }
+    });
   }
 }
